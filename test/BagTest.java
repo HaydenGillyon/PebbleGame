8e364349021df8c8fb49947fecfa294a138430af
jojo <47827326+JorelCoutinho@users.noreply.github.com>
2021-11-08 14:17:16 +0000

    Session END 08/11/2021 (for real 2.0)

--- test/BagTest.java
+++ test/BagTest.java
@@ -6,7 +6,7 @@ public class BagTest {
 
     @Test
     public void testPebbles() {
-        Bag bag = new Bag("TestBag");
+        Bag bag = new BlackBag("TestBag");
         ArrayList<Bag.Pebble> pebbles = new ArrayList<>();
         pebbles.add(new Bag.Pebble(5));
         pebbles.add(new Bag.Pebble(13));
